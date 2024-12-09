2a1052b4b0df7bde9ffa2f8d9cf3e443a606acbf
Albert <finalimzan1@gmail.com>
2024-12-09 13:30:19 +0900

    typeorm 설정

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,32 @@ import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 import { PostModule } from './post/post.module';
 import { InterestModule } from './interest/interest.module';
+import { TypeOrmModule, TypeOrmModuleOptions } from '@nestjs/typeorm';
+import { readFileSync } from 'fs';
+import { DataSource } from 'typeorm';
+import { addTransactionalDataSource } from 'typeorm-transactional';
 
 @Module({
-  imports: [UserModule, PostModule, InterestModule],
+  imports: [
+    UserModule,
+    PostModule,
+    InterestModule,
+    TypeOrmModule.forRootAsync({
+      useFactory: () => {
+        const ormConfig: TypeOrmModuleOptions = JSON.parse(
+          readFileSync(`${__dirname}/../ormconfig.json`, { encoding: 'utf-8' }),
+        );
+        return ormConfig;
+      },
+      async dataSourceFactory(options) {
+        if (!options) {
+          throw new Error('Invalid options passed');
+        }
+
+        return addTransactionalDataSource(new DataSource(options));
+      },
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
