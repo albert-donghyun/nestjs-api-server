5040a8ef674e6106a4685e9ac22e757fea76c8d3
Albert <finalimzan1@gmail.com>
2025-01-15 18:05:29 +0900

    환경 설정

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,12 +3,13 @@ import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { UserModule } from '../user/user.module';
 import { JwtModule } from '@nestjs/jwt';
+import * as config from 'config';
 
 @Module({
   imports: [
     UserModule,
     JwtModule.register({
-      secret: 'secret',
+      secret: config.get<string>('jwt.secret'),
     }),
   ],
   controllers: [AuthController],
--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,13 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { initializeTransactionalContext } from 'typeorm-transactional';
 import { ValidationPipe } from '@nestjs/common';
+import * as config from 'config';
 
 async function bootstrap() {
   initializeTransactionalContext();
 
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
-  await app.listen(3000);
+  await app.listen(config.get('port'));
 }
 bootstrap();
